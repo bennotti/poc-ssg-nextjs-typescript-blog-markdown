56e2b69df9063ab31e7caabc275bf956f5cba98c
Bruno Bennotti <brunobennotti@gmail.com>
2022-06-21 10:41:42 -0300

    feat: estrutura inicial

---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import Head from "next/head";
+import { marked } from "marked";
+import type { NextPage } from 'next'
+import styles from '../../styles/Home.module.css'
+
+const Post: NextPage = (params: any) => {
+    console.log('page', params);
+    return (
+        <>
+        <div className={styles.container}>
+          <Head>
+            <title>{params.data.title}</title>
+            <meta title="description" content={params.data.description} />
+          </Head>
+          <div dangerouslySetInnerHTML={{ __html: params.htmlString }} />
+          </div>
+        </>
+    );
+}
+
+export const getStaticPaths = async () => {
+    const files = fs.readdirSync('posts');
+    console.log('files', files);
+    const paths = files.map(filename => ({
+        params: {
+            slug: filename.replace('.md', '')
+        }
+    }));
+    console.log('paths: ', paths);
+    return {
+        paths,
+        fallback: false,
+    }
+}
+
+export const getStaticProps = async (params: any) => {
+    console.log('params', params);
+    const markdownWithMetadata = fs
+        .readFileSync(path.join('posts', params.params.slug + '.md'))
+        .toString();
+
+    const parsedMarkdown = matter(markdownWithMetadata);
+
+    const htmlString = marked(parsedMarkdown.content);
+    return {
+        props: {
+            htmlString,
+            slug: params.params.slug,
+            data: parsedMarkdown.data
+        }
+    }
+}
+
+export default Post
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
+import fs from "fs";
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
-const Home: NextPage = () => {
+const Home: NextPage = (params: any) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -21,36 +23,16 @@ const Home: NextPage = () => {
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        
+        {params.slugs.map((slug: any) => {
+          return (
+            <div key={slug} className={styles.grid}>
+              <Link href={"/blog/" + slug} className={styles.card}>
+                <a>{"/blog/" + slug}</a>
+              </Link>
+            </div>
+          );
+        })}
       </main>
 
       <footer className={styles.footer}>
@@ -68,5 +50,14 @@ const Home: NextPage = () => {
     </div>
   )
 }
+
+export const getStaticProps = async () => {
+  const files = fs.readdirSync('posts');
+  return {
+    props: {
+      slugs: files.map(filename => filename.replace('.md', ''))
+    }
+  };
+};
 
 export default Home
